e98b2e40f2ca0ff2cfdf3d2bfeb0764582b61109
biancalazzaris <bianca.lazzaris@gmail.com>
2022-03-24 22:03:16 -0300

    aula 24-03

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <h1>Card Componente</h1>
+    <!-- <p>{{ name }}</p>
+    <p>
+      {{ description }}
+    </p> -->
+    <!-- <p>{{person.name}} </p> -->
+  </div>
+</template>
+<script>
+export default {
+  name: 'CardProducts',
+  props: {
+    person: {
+      type: Object,
+      // required: true,
+    },
+  },
+  data() {
+    return {
+      // name: 'Notebook',
+      // description: 'Corei5, 2,6ghz, 8gb, Tela Widescreen 15,4"',
+    };
+  },
+};
+</script>
+<style scoped>
+.card {
+  max-width: 400px;
+  height: 200px;
+  background-color: #eee;
+  border: 1px solid #999;
+  color: black;
+  padding: 10px;
+  margin: 10px;
+}
+
+h1 {
+  color: blue;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="pessoa">
+    <h1>Pessoa:</h1>
+    <label for="name">
+        Nome | {{name}} <br>
+      <input
+      type="text"
+      v-model="name"
+      placeholder="Digite seu nome" />
+    </label>
+    <br>
+    <label for="email">
+        E-mail | {{email}} <br>
+        <input
+        type="text"
+        v-model="email"
+        placeholder="Digite seu e-mail" />
+    </label>
+    <br>
+      <label for="age">
+        Idade | {{age}} <br>
+        <input
+        type="text"
+        v-model="age"
+        placeholder="Digite a sua idade" />
+    </label>
+    <br>
+    <label for="bio">
+        Biografia | {{bio}} <br>
+        <input
+        type="text"
+        v-model="bio"
+        placeholder="Escreva sua biografia" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'Person',
+  data() {
+    return {
+      name: '',
+      email: '',
+      age: '',
+      bio: '',
+      isVisible: false,
+    };
+  },
+};
+</script>
